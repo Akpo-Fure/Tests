3c3bd376f0032614989ffab19d8f52a6438a38af
temybams <temybams06@gmail.com>
2023-06-24 16:49:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const sumOFTwo = (a, b) => {
+//    return a + b
+// }
+
+// console.log(sumOFTwo(1,3))
+
+// const mulOfTwo = (a, b) =>{
+//     return a * b
+// }
+
+
+//Write an array function to calculate distance
+
+const distanceCalc = (speed, time) => {
+    return `Distance = ${speed * time}`
+}
+
+console.log(distanceCalc(50, 6))
